2659b2e6e2ea392e58393428ad5544eef8a39a7e
Aash04 <68348514+Aash04@users.noreply.github.com>
2022-04-19 15:57:58 +0530

    Update DB.py

--- DB.py
+++ DB.py
@@ -4,6 +4,6 @@ con = sqlite3.connect("socialavey.db")
 
 # con.execute("CREATE TABLE SignUp(name varchar(255) ,Lastname varchar(255) ,Username varchar(255) ,email Varchar(255) ,Phone int ,DOB Varchar(255) ,Address varchar(255),password varchar(255),Repassword varchar(255),Gender varchar(255))")
 
-con.execute("CREATE TABLE Posts(pid integer primary key, Utime DATETIME DEFAULT CURRENT_TIMESTAMP, msg text, image text )")
+con.execute("CREATE TABLE NewPost(pid integer primary key,Utime DATETIME DEFAULT CURRENT_TIMESTAMP, username varcchar(255), msg text, image text )")
 
 con.close()
